51a8fac5bd21702cc1623c498f09751d0a06c8c7
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2025-06-24 15:28:56 +0530

    Update dashboard.tsx

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -40,7 +40,7 @@ export function Dashboard() {
   return (
     <div className="space-y-6">
       <div>
-        <h1 className="text-3xl font-bold tracking-tight">Welcome back, Alex</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Welcome back, Nandini </h1>
         <p className="text-muted-foreground mt-1">Let's organize your day together.</p>
       </div>
 
